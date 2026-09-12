19ec05e9c2095e621d10cf679fa8be1eaeab513b
Anjana-krishna-v <krishnavanjana@gmail.com>
2026-09-12 21:49:37 +0530

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+      // int total = 0;
+       //int current = 0;
+       //int start = 0;
+       //int n = gas.length;
+       //for(int i = 0; i < n; i++){
+        //total += gas[i] - cost[i];
+        //current += gas[i] - cost[i];
+        //if (current <0) {
+          //  start = i + 1;
+            //current = 0;
+        //}
+       //} 
+       //return total >= 0 ? start : -1;
+       int total=0;
+       int current =0;
+       int start=0;
+       int n=gas.length;
+       for(int i=0;i<n;i++){
+        total+=gas[i]-cost[i];
+        current+=gas[i]-cost[i];
+        if(current<0){
+            start=i+1;
+            current=0;
+        }
+       }
+       return total>=0 ? start : -1;
+    }
+ 
+}
